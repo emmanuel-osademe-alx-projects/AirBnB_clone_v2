3de255288b198de351a3ebdbf74b9ee4a5299517
emmanuel <mcdecoil@gmail.com>
2022-12-13 23:20:08 +0000

    pep8 fix

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """ Script to start a Flask web application """
-
 from flask import Flask
 
-app =  Flask(__name__)
+app = Flask(__name__)
 app.url_map.strict_slashes = False
+
 
 @app.route('/')
 def hello_hbnb():
-	"""print web"""
-	return 'Hello HBNB!'
+    """print web"""
+    return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
-	app.run(debug=True)
-	app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
